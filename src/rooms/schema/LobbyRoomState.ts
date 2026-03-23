4114b74899f63a37fbef050cd16491399e04f019
Abdon <artistarulbabu@gmail.com>
2026-03-23 16:35:59 +0530

    clean

--- src/rooms/schema/LobbyRoomState.ts
+++
@@ -1,8 +0,0 @@
-import { Schema, type } from "@colyseus/schema";
-
-export class LobbyRoomState extends Schema {
-  @type("uint16") playersWaiting: number = 0;
-  @type("float32") averageWaitTimeSeconds: number = 0;
-  @type("string") matchmakingStatus: string = "idle";
-  @type("string") messageOfTheDay: string = "Welcome to Power Cricket!";
-}

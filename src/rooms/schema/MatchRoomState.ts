bc780dcb08203b4939f75e174f7b543656bce454
Abdon <artistarulbabu@gmail.com>
2026-04-03 22:03:33 +0530

    upadten5

--- src/rooms/schema/MatchRoomState.ts
+++ src/rooms/schema/MatchRoomState.ts
@@ -12,39 +12,39 @@ export class TeamPlayer extends Schema {
     @type("string")  rarity:    string = "";   // [Type(3)]
     @type("string")  powerType: string = "";   // [Type(4)]
     @type("number")  basePower: number = 1;    // [Type(5)]
-    @type("number")  level:     number = 1;    // [Type(6)]
+    @type("int32")   level:     number = 1;    // [Type(6)]
 }
 
 export class PowerUsage extends Schema {
     @type("string")  powerId:       string  = "";    // [Type(0)]
     @type("string")  playerCardId:  string  = "";    // [Type(1)]
     @type("string")  playerId:      string  = "";    // [Type(2)]
-    @type("number")  maxUses:       number  = 1;     // [Type(3)]
-    @type("number")  usesConsumed:  number  = 0;     // [Type(4)]
+    @type("int32")   maxUses:       number  = 1;     // [Type(3)]
+    @type("int32")   usesConsumed:  number  = 0;     // [Type(4)]
     @type("boolean") activeThisBall: boolean = false; // [Type(5)]
 }
 
 export class BallState extends Schema {
-    @type("number")  ballNumber:     number = 0;  // [Type(0)]
+    @type("int32")   ballNumber:     number = 0;  // [Type(0)]
     @type("string")  outcome:        string = "";  // [Type(1)] dot|run|wicket
-    @type("number")  runs:           number = 0;  // [Type(2)]
+    @type("int32")   runs:           number = 0;  // [Type(2)]
     @type("string")  bowlerType:     string = "";  // [Type(3)] fast|spin
     @type("string")  bowlerPlayerId:   string = "";  // [Type(4)]
     @type("string")  batsmanPlayerId:  string = "";  // [Type(5)]
-    @type("number")  sliderPosition: number = 0;  // [Type(6)] 0-100
+    @type("int32")   sliderPosition: number = 0;  // [Type(6)] 0-100
     @type("string")  powerUsed:      string = "";  // [Type(7)]
     @type("number")  arrowSpeed:     number = 1;  // [Type(8)]
-    @type("number")  originalRuns:   number = 0;  // [Type(9)]
+    @type("int32")   originalRuns:   number = 0;  // [Type(9)]
 }
 
 export class InningsData extends Schema {
     @type("string")    battingPlayerId: string  = "";   // [Type(0)]
     @type("string")    bowlingPlayerId: string  = "";   // [Type(1)]
-    @type("number")    score:           number  = 0;    // [Type(2)]
-    @type("number")    wickets:         number  = 0;    // [Type(3)]
-    @type("number")    ballsBowled:     number  = 0;    // [Type(4)]
-    @type("number")    currentOver:     number  = 0;    // [Type(5)]
-    @type("number")    target:          number  = -1;   // [Type(6)] -1 until innings 2
+    @type("int32")     score:           number  = 0;    // [Type(2)]
+    @type("int32")     wickets:         number  = 0;    // [Type(3)]
+    @type("int32")     ballsBowled:     number  = 0;    // [Type(4)]
+    @type("int32")     currentOver:     number  = 0;    // [Type(5)]
+    @type("int32")     target:          number  = -1;   // [Type(6)] -1 until innings 2
     @type([BallState]) balls = new ArraySchema<BallState>();  // [Type(7)]
     @type("boolean")   isComplete:      boolean = false; // [Type(8)]
 }
@@ -54,14 +54,14 @@ export class PowerSlot extends Schema {
     @type("string")  powerId:       string  = "";    // [Type(1)]
     @type("string")  playerCardId:  string  = "";    // [Type(2)]
     @type("boolean") active:        boolean = false; // [Type(3)]
-    @type("number")  usesRemaining: number  = 1;     // [Type(4)]
+    @type("int32")   usesRemaining: number  = 1;     // [Type(4)]
 }
 
 export class PlayerState extends Schema {
     @type("string")   sessionId:           string  = "";    // [Type(0)]
     @type("string")   playerId:            string  = "";    // [Type(1)]
     @type("string")   name:                string  = "";    // [Type(2)]
-    @type("number")   elo:                 number  = 1000;  // [Type(3)]
+    @type("int32")    elo:                 number  = 1000;  // [Type(3)]
     @type("string")       teamId:                string  = "";    // [Type(4)]
     @type("boolean")      connected:             boolean = true;  // [Type(5)]
     @type("boolean")      ready:                 boolean = false; // [Type(6)]
@@ -79,9 +79,9 @@ export class MatchRoomState extends Schema {
     @type("string")  tossChoice: string  = "";      // [Type(3)] bat|bowl
     @type("string")  tossCaller: string  = "";      // [Type(4)]
 
-    @type("number")  oversPerMatch:    number  = 3;     // [Type(5)]
-    @type("number")  ballsPerOver:     number  = 6;     // [Type(6)]
-    @type("number")  maxWickets:       number  = 10;    // [Type(7)]
+    @type("int32")   oversPerMatch:    number  = 3;     // [Type(5)]
+    @type("int32")   ballsPerOver:     number  = 6;     // [Type(6)]
+    @type("int32")   maxWickets:       number  = 10;    // [Type(7)]
     @type("boolean") superOverEnabled: boolean = true;  // [Type(8)]
 
     @type({ map: PlayerState }) players = new MapSchema<PlayerState>(); // [Type(9)]
@@ -96,7 +96,7 @@ export class MatchRoomState extends Schema {
 
     @type("string")  winner:    string  = "";  // [Type(16)]
     @type("string")  winReason: string  = "";  // [Type(17)] chase|defended|super_over|super_over_wickets|forfeit|disconnect|draw
-    @type("number")  eloDelta:  number  = 0;   // [Type(18)]
+    @type("int32")   eloDelta:  number  = 0;   // [Type(18)]
 
     @type("boolean") isPrivate: boolean = false; // [Type(19)]
     @type("string")  roomCode:  string  = "";    // [Type(20)]
